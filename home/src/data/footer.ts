011f716402941867c0264184746dcbb4c204ff49
itshirdeshk <hirdeshkhandelwal58@gmail.com>
2025-06-09 23:35:24 +0530

    fix: update home

--- home/src/data/footer.ts
+++ home/src/data/footer.ts
@@ -4,14 +4,29 @@ import institute16 from "../assets/images/patners/partner_logo_16.jpg"
 import institute17 from "../assets/images/patners/partner_logo_17.jpg"
 import institute18 from "../assets/images/patners/partner_logo_18.jpg"
 import institute19 from "../assets/images/patners/partner_logo_19.jpg"
-// import institute10 from "../assets/images/patners/patner_logo_10.png"
-// import institute11 from "../assets/images/patners/patner_logo_11.jpg"
+import institute20 from "../assets/images/patners/partner_logo_20.jpg"
+import institute21 from "../assets/images/patners/partner_logo_21.jpg"
+import institute22 from "../assets/images/patners/partner_logo_22.jpg"
+import institute23 from "../assets/images/patners/partner_logo_23.jpg"
+import institute24 from "../assets/images/patners/partner_logo_24.jpg"
+import institute25 from "../assets/images/patners/partner_logo_25.jpg"
+import institute26 from "../assets/images/patners/partner_logo_26.jpg"
+import institute27 from "../assets/images/patners/partner_logo_27.jpg"
+
 
 export const institutes = [
+    { src: institute20, alt: "Institute 20" },
+    { src: institute21, alt: "Institute 21" },
     { src: institute14, alt: "Institute 14" },
     { src: institute15, alt: "Institute 15" },
-    { src: institute16, alt: "Institute 16" },
-    { src: institute17, alt: "Institute 17" },
     { src: institute18, alt: "Institute 18" },
     { src: institute19, alt: "Institute 19" },
+    { src: institute16, alt: "Institute 16" },
+    { src: institute17, alt: "Institute 17" },
+    { src: institute22, alt: "Institute 22" },
+    { src: institute23, alt: "Institute 23" },
+    { src: institute24, alt: "Institute 24" },
+    { src: institute25, alt: "Institute 25" },
+    { src: institute26, alt: "Institute 26" },
+    { src: institute27, alt: "Institute 27" },
 ];
